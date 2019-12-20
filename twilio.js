e2a54e890a1558f4c8d705b3f742f923516a565c
Tariq Khan <45430120+Thk101@users.noreply.github.com>
2019-12-20 14:53:29 -0500

    Update twilio.js

--- twilio.js
+++ twilio.js
@@ -1,6 +1,6 @@
 const twilio = require("twilio");
 
-const accountSid = "ACb4529575c64c323245a4eabf980429d4";
-const authToken = "e37b744fb399989527689135ba1c4125";
+const accountSid = "";
+const authToken = "";
 
 module.exports = new twilio.Twilio(accountSid, authToken);
